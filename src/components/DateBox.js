5c2390ff260ce47f1af678aef9ce3afb253a2313
Anirban Aditya Halder <nabovtl@gmail.com>
2024-09-30 12:24:17 +0530

    lmao

--- src/components/DateBox.js
+++ src/components/DateBox.js
@@ -1,5 +1,5 @@
 export default function DateBox({name, placeholder}){
     return(
-        <input type="date" name={name} placeholder={placeholder} className="bg-[#F3F1FF] dark:bg-[#252525] focus:scale-[1.02] ease-out duration-150 "/>
+        <input type="date" name={name} id={name} placeholder={placeholder} className="bg-[#F3F1FF] dark:bg-[#252525] focus:scale-[1.02] ease-out duration-150 "/>
     )
 }
--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,5 +1,5 @@
 export default function TextBox({name, placeholder, id}){
     return(
-        <input id={id} name={name} placeholder={placeholder} className="bg-[#F3F1FF] dark:bg-[#252525] focus:scale-[1.02] ease-out duration-150" type="text"/>
+        <input id={name} name={name} placeholder={placeholder} className="bg-[#F3F1FF] dark:bg-[#252525] focus:scale-[1.02] ease-out duration-150" type="text"/>
     )
 }
